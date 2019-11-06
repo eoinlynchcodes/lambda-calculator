2b778580b1f5a457aa6a9c67659bdf155cac8bbf
eoinlynchcodes <eoin95lynch@gmail.com>
2019-11-06 12:19:02 +0000

    For Submission

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import { Numbers } from './Numbers';
 
-const NumberButton = () => {
+export const NumberButton = () => {
   return (
     <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <Numbers/>
     </>
   );
 };
+
